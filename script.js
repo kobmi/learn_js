85e339b87560fc5651e0ca4a024b50cf6e83f6ed
Sergey <sergeykolesnichenko@ukr.net>
2020-07-29 17:20:34 +0300

    practice part4

--- script.js
+++ script.js
@@ -349,4 +349,168 @@ P.S. Функции вызывать не обязательно*/
 // console.log(arr2.sort(compare));
 // function compare(a, b) {
 //   return b - a;
+//
+// -------------------------------------Spread--------------------------------------
+
+// let arr1 = [1, 2, 3, 4];
+// let arr2 = [0, ...arr1, 5, 6];
+// console.log(arr2);
+// let arr3 = [...arr1, ...arr2];
+// console.log(arr3);
+
+// function func(s1, s2, s3, s4, s5) {
+//   return `${s1}-${s2}-${s3}-${s4}-${s5}`;
 // }
+
+// let str = "Hello";
+// console.log(...str);
+// console.log([...str]);
+
+// console.log(func(..."Hello"));
+
+// function sum(...nums) {
+//   let sum = 0;
+//   for (let num of nums) {
+//     sum += num;
+//   }
+//   return sum;
+// }
+// console.log(sum(1, 2, 3, 5, 6));
+// Напишите функцию, которая будет принимать параметрами произвольное количество чисел и возвращать их среднее арифметическое (сумма делить на количество).
+
+// function avar(...numbers) {
+//   let avarage = 0;
+//   for (let num of numbers) {
+//     avarage += num;
+//   }
+//   return avarage / numbers.length;
+// }
+
+// console.log(avar(5, 10, 15));
+
+// function unit(...arrs) {
+//   return arrs;
+// }
+
+// console.log(unit([1, 2, 3], [4, 5, 6], [7, 8, 9]));
+
+// let arrs = [
+//   [1, 2, 3],
+//   [5, 6, 7],
+//   [8, 9, 10],
+// ];
+
+// let result = [].concat(...arrs);
+// console.log(result);
+
+// ---------------------------------------------ООП-----------------------------------
+
+// let str = "some";
+// let strObj = new String(str);
+// console.log(typeof str);
+// console.log(typeof strObj);
+// console.dir([1, 2, 3]);
+
+// Object.setPrototypeOf(новый обьект, прототип от которого наследуемся)
+// Object.create(протоип от которого наследуемся)
+
+// const soldier = {
+//   health: 400,
+//   armor: 100,
+//   sayHello: function () {
+//     console.log("Hello");
+//   },
+// };
+
+// const jonh = Object.create(soldier);
+
+// jonh.sayHello();
+
+// ----------------------------------------ПРАКТИКА ч4------------------------------------
+/* Задание на урок:
+
+1) У нас уже есть рабочее приложение, состоящее из отдельных функций. Представьте, что
+перед вами стоит задача переписать его так, чтобы все функции стали методами объекта personalMovieDB
+Такое случается в реальных продуктах при смене технологий или подхода к архитектуре программы
+
+2) Создать метод toggleVisibleMyDB, который при вызове будет проверять свойство privat. Если оно false - он
+переключает его в true, если true - переключает в false. Протестировать вместе с showMyDB.
+
+3) В методе writeYourGenres запретить пользователю нажать кнопку "отмена" или оставлять пустую строку. 
+Если он это сделал - возвращать его к этому же вопросу. После того, как все жанры введены - 
+при помощи метода forEach вывести в консоль сообщения в таком виде:
+"Любимый жанр #(номер по порядку, начиная с 1) - это (название из массива)"*/
+
+// Код возьмите из предыдущего домашнего задания
+
+let numbersOfFilms;
+
+const personalMevieDB = {
+  count: numbersOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+  start: function () {
+    numbersOfFilms = Number(prompt("Сколько фильмов вы уже посмотрели?", ""));
+    while (
+      numbersOfFilms == "" ||
+      numbersOfFilms == null ||
+      isNaN(numbersOfFilms)
+    ) {
+      numbersOfFilms = Number(prompt("Сколько фильмов вы уже посмотрели?", ""));
+    }
+  },
+  rememberMyFilms: function () {
+    for (let i = 0; i < 2; i++) {
+      const a = prompt("Один из последних просмотренных фильмов?"),
+        b = prompt("На сколько оцените его?");
+
+      if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+        personalMevieDB.movies[a] = b;
+        console.log("yes");
+      } else {
+        console.log("error");
+        i--;
+      }
+    }
+  },
+  detectPersonalLevel: function () {
+    if (personalMevieDB.count < 10) {
+      console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMevieDB.count >= 10 && personalMevieDB.count < 30) {
+      console.log("Вы классический зритель");
+    } else if (personalMevieDB.count >= 30) {
+      console.log("Вы киноман");
+    } else {
+      console.log("Произошла ошибка");
+    }
+  },
+  sshowMyDB: function (hidden) {
+    if (!hidden) {
+      console.log(personalMevieDB);
+    }
+  },
+  writeYourGenres: function () {
+    for (let i = 1; i <= 3; i++) {
+      personalMevieDB.genres[i - 1] = prompt(
+        `Ваш любимый жанр под номером ${i}`
+      );
+    }
+  },
+  toggleVisibleMyDB: function () {
+    if (personalMevieDB.privat === false) {
+      personalMevieDB.privat = true;
+    } else {
+      personalMevieDB.privat = false;
+    }
+  },
+};
+
+
+// personalMevieDB.start();
+// rememberMyFilms();
+// detectPersonalLevel();
+// writeYourGenres();
+// writeYourGenres();
+// showMyDB(personalMevieDB.privat);
