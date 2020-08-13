eb0178482325e1ee46a16860f879fc0a496c036c
Sergey <sergeykolesnichenko@ukr.net>
2020-08-13 16:07:36 +0300

    arrays reduce

--- advanced_js/20_arrays_map_filter/20_arrays.js
+++ advanced_js/20_arrays_map_filter/20_arrays.js
@@ -1,6 +1,6 @@
 "use stricts";
 
-// filter - возвращает новый массив
+// .filter - возвращает новый массив эелементов которые соответствуют условию
 // const names = ["Ivan", "Ann", "Ksenia", "Voldemart"];
 
 // const shortNames = names.filter((name) => {
@@ -10,7 +10,7 @@
 // console.log(shortNames);
 
 // =============================================================================
-// map = изменет
+// .map = возвращает новый массив с измененными элементами
 // const answers = ["IvaN", "AnnA", "HellO"].map((answer) =>
 //   answer.toLocaleLowerCase()
 // );
@@ -20,8 +20,55 @@
 // console.log(answers);
 
 // =============================================================================
-// some
-
+// .some - возвращает true если хотя бы один элемент соответсвует условию
 // const arr = [4, "hello", "str"];
 // console.log(arr.some((item) => typeof item === "number"));
 
+// =======================================================================
+// .every возвращает true если все элементы соответсвует условию
+
+// const arr = [1, 2, 3, 4, 5];
+// console.log(
+//   arr.every((item) => {
+//     return typeof item === "number";
+//   })
+// );
+
+// ==========================================================================
+// .reduce
+
+// const arr = [4, 5, 1, 3, 2, 6];
+// // 0        4
+// // 4        5
+// // 9        1
+// // 10       3
+// // 13       2
+// // 15       6
+// // 21
+
+// const result = arr.reduce((sum, current) => {
+//   return sum + current;
+// });
+
+// console.log(result); // 21
+
+// const arr2 = ["apple", "pear", "plum"];
+
+// const res = arr2.reduce((sum, current) => {
+//   return `${sum}, ${current}`;
+// });
+// console.log(res); // apple, pear, plum
+
+//obj.entries
+
+const obj = {
+  ivan: "persone",
+  ann: "persone",
+  dog: "animal",
+  cat: "animal",
+};
+
+const newArr = Object.entries(obj)
+  .filter((elem) => elem[1] === "persone")
+  .map((elem) => elem[0]);
+console.log(newArr);
